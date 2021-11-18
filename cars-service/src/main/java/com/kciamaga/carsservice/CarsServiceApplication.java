d1b5b03e3b30d0eafda6d313f77bd5dbade6f789
Krystian Ciamaga <krystian.ciamaga@ext.avenga.com>
2021-11-18 12:27:38 +0100

    cars service added

---
+++ cars-service/src/main/java/com/kciamaga/carsservice/CarsServiceApplication.java
@@ -0,0 +1,15 @@
+package com.kciamaga.carsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CarsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsServiceApplication.class, args);
+    }
+
+}
